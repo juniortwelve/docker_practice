64b580c8b340237f12bc38c17847f4fadd239fad
junior <bluehawaiiouhu@gmail.com>
2021-05-05 23:34:50 +0900

    fix the codes to pass rubocop

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   describe 'Associations' do
     describe 'presence' do
       it { is_expected.to validate_presence_of(:name) }
-      it {is_expected.to validate_presence_of(:email)    }
+      it { is_expected.to validate_presence_of(:email) }
     end
   end
 end
