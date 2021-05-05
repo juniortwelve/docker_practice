62ed8fccdaa5152c880f41bc8dc30c8cb33b317e
junior <bluehawaiiouhu@gmail.com>
2021-05-05 13:31:23 +0900

    add user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    describe 'presence' do
+      it { is_expected.to validate_presence_of(:name) }
+      it { is_expected.to validate_presence_of(:email)}
+    end
+  end
 end
